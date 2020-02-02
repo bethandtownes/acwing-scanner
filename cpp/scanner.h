ea9e287ff6ef01942c6d8098e35ac1ad3c01249d
jasonsun0310 <ds653@cornell.edu>
2020-02-02 10:39:09 -0500

    init

--- cpp/scanner.h
+++ cpp/scanner.h
@@ -6,14 +6,29 @@
 // using namespace std;
 
 namespace acwing {
-template<class T> struct is_vector { using type = T ; constexpr static bool value = false;};
-template<class T> struct is_vector<std::vector<T>> { using type = std::vector<T> ; constexpr  static bool value = true;};
-template<typename T> inline constexpr bool is_vector_v = is_vector<T>::value ;
+template <class T>
+struct is_vector {
+  using type                  = T;
+  constexpr static bool value = false;
+};
+template <class T>
+struct is_vector<std::vector<T>> {
+  using type                  = std::vector<T>;
+  constexpr static bool value = true;
+};
+template <typename T>
+inline constexpr bool is_vector_v = is_vector<T>::value;
 
-template<class T> struct is_pair {constexpr static bool value = false;};
-template<class T1, class T2> struct is_pair<std::pair<T1, T2>> { using type = std::pair<T1, T2> ; constexpr  static bool value = true;};
-template<typename T> inline constexpr bool is_pair_v = is_pair<T>::value;
-}
+template <class T>
+struct is_pair { constexpr static bool value = false; };
+template <class T1, class T2>
+struct is_pair<std::pair<T1, T2>> {
+  using type                  = std::pair<T1, T2>;
+  constexpr static bool value = true;
+};
+template <typename T>
+inline constexpr bool is_pair_v = is_pair<T>::value;
+}  // namespace acwing
 
 class input_reader {
  private:
@@ -28,21 +43,21 @@ class input_reader {
   input_reader(std::istream& input_stream = std::cin) : cur_pos_(0),
                                                         num_chars_(0),
                                                         auto_reload_(true),
-                                                        input_stream_(input_stream) {};
+                                                        input_stream_(input_stream){};
 
   inline char peek() { return ready(), buffer_[cur_pos_]; };
 
-  template<class data_t, typename std::enable_if_t<std::is_same_v<char, data_t>>* = nullptr>
+  template <class data_t, typename std::enable_if_t<std::is_same_v<char, data_t>>* = nullptr>
   inline data_t read() {
     return (refresh_buffer(), ready(), _read());
   }
 
-  template<class data_t, typename std::enable_if_t<not std::is_same_v<char, data_t> and std::is_integral_v<data_t>>* = nullptr>
+  template <class data_t, typename std::enable_if_t<not std::is_same_v<char, data_t> and std::is_integral_v<data_t>>* = nullptr>
   data_t read() {
     ready();
     const data_t sign = (std::is_signed_v<data_t> and peek() == '-') ? ((void)read<char>(), -1) : 1;
-    const data_t val = [&](data_t acc = 0) {
-       for (char ch = _read(); not is_space(ch); ch = _read()) {
+    const data_t val  = [&](data_t acc = 0) {
+      for (char ch = _read(); not is_space(ch); ch = _read()) {
         acc = (acc * 10) + ch - '0';
       }
       return acc;
@@ -50,7 +65,7 @@ class input_reader {
     return sign * val;
   };
 
-  template<class data_t, typename std::enable_if_t<std::is_same_v<std::string, data_t>>* = nullptr>
+  template <class data_t, typename std::enable_if_t<std::is_same_v<std::string, data_t>>* = nullptr>
   data_t read() {
     ready();
     const data_t val = [&](data_t acc = {}) {
@@ -77,7 +92,7 @@ class input_reader {
     ready();
     const auto val = [&](data_t self = {}) {
       auto_reload_ = false;
-      while(ready())
+      while (ready())
         self.emplace_back(read<typename data_t::value_type>());
       auto_reload_ = true;
       return self;
@@ -87,17 +102,16 @@ class input_reader {
 
   template <class data_t, typename std::enable_if_t<acwing::is_vector_v<data_t>>* = nullptr>
   data_t read(std::size_t n) {
-      ready();
-      const auto val = [&](data_t self = {}) {
-        for (int i = 0; i < n; ++i) 
-          self.emplace_back(read<typename data_t::value_type>());
-        return self;
-      }();
-      return val;
+    ready();
+    const auto val = [&](data_t self = {}) {
+      for (int i = 0; i < n; ++i)
+        self.emplace_back(read<typename data_t::value_type>());
+      return self;
+    }();
+    return val;
   }
 
-  template <class data_t, typename std::enable_if_t<acwing::is_vector_v<data_t>
-                                                    and acwing::is_vector_v<typename data_t::value_type>>* = nullptr>
+  template <class data_t, typename std::enable_if_t<acwing::is_vector_v<data_t> and acwing::is_vector_v<typename data_t::value_type>>* = nullptr>
   data_t read(std::size_t n, std::size_t m) {
     ready();
     const auto val = [&](data_t self = {}) {
@@ -111,15 +125,17 @@ class input_reader {
 
   template <class data_t, typename std::enable_if_t<acwing::is_pair_v<data_t>>* = nullptr>
   data_t read() {
-    return data_t{read<typename data_t::first_type>(), read<typename data_t::second_type>()};
+    const auto fst = read<typename data_t::first_type>();
+    const auto snd = read<typename data_t::second_type>();
+    return data_t{fst, snd};
   }
 
-  template<class data_t, typename std::enable_if_t<std::is_floating_point_v<data_t>>* = nullptr>
+  template <class data_t, typename std::enable_if_t<std::is_floating_point_v<data_t>>* = nullptr>
   data_t read() {
     ready();
     const data_t sign = (std::is_signed_v<data_t> and peek() == '-') ? ((void)read<char>(), -1.0) : 1.0;
-    
-    const auto [before_decimal, has_decimal] = [&](data_t acc = 0.0) -> std::pair<data_t, bool>{
+
+    const auto [before_decimal, has_decimal] = [&](data_t acc = 0.0) -> std::pair<data_t, bool> {
       for (char ch = _read(); not is_space(ch); ch = _read()) {
         acc = (acc * 10.0) + ch - '0';
         if (peek() == '.')
@@ -127,17 +143,17 @@ class input_reader {
       }
       return {acc, false};
     }();
-    
+
     const data_t after_decimal = [&](data_t acc = 0.0) {
-      if (not has_decimal) 
+      if (not has_decimal)
         return acc;
-      else {      
-        for (auto [ch, base] = pair(((void)read<char>(), _read()), data_t{1.0 / 10.0}); not is_space(ch); ch = _read(), base /= 10) 
+      else {
+        for (auto [ch, base] = pair(((void)read<char>(), _read()), data_t{1.0 / 10.0}); not is_space(ch); ch = _read(), base /= 10)
           acc += (ch - '0') * base;
         return acc;
       }
     }();
-    
+
     return before_decimal + after_decimal;
   }
 
@@ -148,12 +164,12 @@ class input_reader {
       input_stream_.getline(buffer_, BUFFER_SIZE);
       num_chars_ = std::cin.gcount();
     }
-   }
+  }
 
   inline char _read() { return refresh_buffer(), buffer_[cur_pos_++]; };
-  
-  inline bool is_space (char ch) { return ch == ' ' or ch == '\n' or ch == '\t' or ch == '\0'; };
-  
+
+  inline bool is_space(char ch) { return ch == ' ' or ch == '\n' or ch == '\t' or ch == '\0'; };
+
   inline bool ready() {
     refresh_buffer();
     if (not auto_reload_ and cur_pos_ >= num_chars_)
@@ -168,9 +184,9 @@ class input_reader {
         cur_pos_++;
       return true;
     }
-    else throw std::domain_error("");
+    else
+      throw std::domain_error("");
   }
-  
 
   // void debug(string level) {
   //   const auto view = [&](vector<string> self = {}) {
@@ -186,7 +202,7 @@ class input_reader {
   //     });
   //     return self;
   //   }();
-    
+
   //   cout << level
   //        << see(view)
   //        << see(cur_pos_)
@@ -196,6 +212,4 @@ class input_reader {
   // }
 };
 
-
-
-#endif //SCANNER_H
+#endif  //SCANNER_H
